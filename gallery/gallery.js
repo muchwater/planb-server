cec33454830b0bed5eabe715e1d7df14e176e1c0
muchwater <muchwater@kaist.ac.kr>
2022-07-18 22:18:45 +0900

    test

---
+++ gallery/gallery.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// let list__video = document.querySelectorAll('.main__list .card');
+// let main__video = document.querySelector('.main__1 video');
+// let title = document.querySelector('.main__1 h1');
+// let content = document.querySelector('.main__1 p');
+
+// list__video.forEach(video => {
+//     video.onclick = () => {
+//         list__video.forEach(card => card.classList.remove('active'));
+//         video.classList.add('active');
+//         if (video.classList.contains('active')) {
+//             let src = video.children[0].getAttribute('src');
+//             main__video.src = src;
+//             let text = video.children[1].innerHTML;
+//             title.innerHTML = text;
+//         };
+//     };
+// });
